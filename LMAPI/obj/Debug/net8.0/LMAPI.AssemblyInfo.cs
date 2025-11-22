6be92cc859c308b8588c17747dd93c77aea3f88b
FelippeSalvoDev <felippesalvo.dev@gmail.com>
2025-11-21 21:16:34 -0300

    arrumano assyemblies pra n ndar conlfito no banco

--- LMAPI/obj/Debug/net8.0/LMAPI.AssemblyInfo.cs
+++ LMAPI/obj/Debug/net8.0/LMAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3ced36adb06fa18ea8c05b289288e5f8f127796")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2da32914f7fba878ccb0ebfc481bfcd75fca0e99")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
